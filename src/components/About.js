b31da5332e7649a26f0a4fc8aeadcf06ecd0dfea
gabefando <gabe.fonseca.04@gmail.com>
2022-08-24 09:09:45 -0400

    switched from tachyons to bootstrapped

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,14 @@ import React from "react";
 
 function About() {
     return(
-        <div className="bg-light-silver black">
+        <div className="mt-3 mb-3 text-center">
             <h1>Hi, my name is Gabriel Fonseca</h1>
             <h2>And i'm a full-stack web developer.</h2>
-            <p className="mw7">Im a full-stack web developer living in Atlanta Georgia, passionate about programming and developing websites. I am recently doing a coding bootcamp with Georgia Tech and have experience with HTML, CSS, JavaScript, Bootstrap, Node.js, and React. I have always been someone who has both a creative and a logical side, and web development is the perfect area for me to use both of those skills.</p>
+            <div className="container mt-3">
+                <div className="row justify-content-center">
+                    <p className="col-7">Im a full-stack web developer living in Atlanta Georgia, passionate about programming and developing websites. I am recently doing a coding bootcamp with Georgia Tech and have experience with HTML, CSS, JavaScript, Bootstrap, Node.js, and React. I have always been someone who has both a creative and a logical side, and web development is the perfect area for me to use both of those skills.</p>
+                </div>  
+            </div>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Navbar() {
     return(
-        <div className="pa3 bg-near-black light-silver">
-            <ul className="flex justify-center list">
-                <li className="ph4">About</li>
-                <li className="ph4">Projects</li>
-                <li className="ph4">Articles</li>
-                <li className="ph4">Links</li>
-                <li className="ph4">Resume</li>
+        <div className="bg-dark text-light sticky-top">
+            <ul className="nav justify-content-center">
+                <li className="px-4">About</li>
+                <li className="px-4">Projects</li>
+                <li className="px-4">Articles</li>
+                <li className="px-4">Links</li>
+                <li className="px-4">Resume</li>
             </ul>
         </div>
     );
